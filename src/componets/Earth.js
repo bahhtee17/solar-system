8e99ea6f5f5712548399810e62938bf842499058
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-01-29 03:05:20 +0500

    catch error method is added

--- src/componets/Earth.js
+++ src/componets/Earth.js
@@ -3,12 +3,19 @@ import EarthItem from "./PlanetItems/EarthItem";
 
 const Earth = () => {
   const [planets, setPlanets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       const response = await fetch(
         "https://solar-sytem-project-default-rtdb.europe-west1.firebasedatabase.app/planets.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
       console.log(responseData);
       const loadedData = [];
@@ -25,27 +32,26 @@ const Earth = () => {
         });
 
         setPlanets(loadedData);
+        setIsLoading(false);
       }
     };
-    fetchData();
+    fetchData().catch((error) => setError(error.message));
   }, []);
-  return (
-    <>
-      {planets.map((planet) => (
-        <EarthItem
-          key={planet.id}
-          id={planet.id}
-          name={planet.name}
-          nameSake={planet.nameSake}
-          formation={planet.formation}
-          orbitAndRotation={planet.orbitAndRotation}
-          sizeAndDistance={planet.sizeAndDistance}
-          atmosphere={planet.atmosphere}
-          surface={planet.surface}
-        />
-      ))}
-    </>
-  );
+
+  const planet = planets.map((planet) => (
+    <EarthItem
+      key={planet.id}
+      id={planet.id}
+      name={planet.name}
+      nameSake={planet.nameSake}
+      formation={planet.formation}
+      orbitAndRotation={planet.orbitAndRotation}
+      sizeAndDistance={planet.sizeAndDistance}
+      atmosphere={planet.atmosphere}
+      surface={planet.surface}
+    />
+  ));
+  return <> {error ? error : isLoading ? <p>Loading...</p> : planet} </>;
 };
 
 export default Earth;
--- src/componets/PlanetItems/EarthItem.js
+++ src/componets/PlanetItems/EarthItem.js
@@ -4,6 +4,14 @@ const EarthItem = (props) => {
   return (
     <div>
       <h1>{props.name}</h1>
+      <iframe
+        width='962'
+        height='541'
+        src='https://www.youtube.com/embed/JGXi_9A__Vc'
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen></iframe>
       <p>{props.nameSake}</p>
       <p>{props.formation}</p>
       <p>{props.orbitAndRotation}</p>
